8dd521a97d6382e8abf1b8f36a6604ecae5f1110
skad12 <stephenkunle26@gmail.com>
2025-08-20 17:12:12 +0100

    next image config

--- api/Projects.js
+++ api/Projects.js
@@ -1,97 +1,76 @@
-import pImg1 from '/public/images/project/4.jpg'
-import pImg2 from '/public/images/project/5.jpg'
-import pImg3 from '/public/images/project/6.jpg'
-import pImg4 from '/public/images/project/7.jpg'
-import pImg5 from '/public/images/project/1.jpg'
-import pImg6 from '/public/images/project/2.jpg'
-import pImg7 from '/public/images/project/3.jpg'
-
-
-import pSImg1 from '/public/images/project-single/img-1.jpg'
-import pSImg2 from '/public/images/project-single/img-2.jpg'
-import pSImg3 from '/public/images/project-single/img-3.jpg'
-import pSImg4 from '/public/images/project-single/img-4.jpg'
-import pSImg5 from '/public/images/project-single/img-5.jpg'
-import pSImg6 from '/public/images/project-single/img-6.jpg'
-import pSImg7 from '/public/images/project-single/img-7.jpg'
-
-import sSingleimg1 from '/public/images/project-single/2.jpg'
-import sSingleimg2 from '/public/images/project-single/3.jpg'
-
-
 const Projects = [
-    {
-        Id: '1',
-        pTitle: 'Architecture', 
-        slug: 'Architecture',
-        pImg:pImg1,
-        pSImg:pSImg1,
-        ssImg1:sSingleimg1,
-        ssImg2:sSingleimg2,
-    },
-    {
-        Id: '2',
-        pTitle: 'Exterior Design', 
-        slug: 'Exterior-Design',
-        pImg:pImg2,
-        pSImg:pSImg2,
-        ssImg1:sSingleimg1,
-        ssImg2:sSingleimg2,
-    },
-    {
-        Id: '3',
-        pTitle: 'Interior', 
-        slug: 'Interior',
-        pImg:pImg3,
-        pSImg:pSImg3,
-        ssImg1:sSingleimg1,
-        ssImg2:sSingleimg2,
-    },
-    {
-        Id: '4',
-        pTitle: 'Furniture & Decor', 
-        slug: 'Furniture-&-Decor',
-        pImg:pImg4,
-        pSImg:pSImg4,
-        ssImg1:sSingleimg1,
-        ssImg2:sSingleimg2,
-    },
-    {
-        Id: '5',
-        pTitle: 'Modern Architecture in London', 
-        slug: 'Modern-Architecture-in-London',
-        pImg:pImg5,
-        pSImg:pSImg5,
-        ssImg1:sSingleimg1,
-        ssImg2:sSingleimg2,
-    },
-    {
-        Id: '6',
-        pTitle: 'Interior Design in San Francisco', 
-        slug: 'Interior-Design-in-San-Francisco',
-        pImg:pImg6,
-        pSImg:pSImg6,
-        ssImg1:sSingleimg1,
-        ssImg2:sSingleimg2,
-    },
-    {
-        Id: '7',
-        pTitle: 'Exclusive Home Decor in California', 
-        slug: 'Exclusive-Home-Decor-in-California',
-        pImg:pImg7,
-        pSImg:pSImg7,
-        ssImg1:sSingleimg1,
-        ssImg2:sSingleimg2,
-    },
-    {
-        Id: '8',
-        pTitle: 'Interior Design in San Francisco', 
-        slug: 'Interior-Design-in-San-Francisco',
-        pImg:pImg6,
-        pSImg:pSImg6,
-        ssImg1:sSingleimg1,
-        ssImg2:sSingleimg2,
-    },
-]
+  {
+    Id: "1",
+    pTitle: "Architecture",
+    slug: "Architecture",
+    pImg: "/images/project/4.jpg",
+    pSImg: "/images/project-single/img-1.jpg",
+    ssImg1: "/images/project-single/2.jpg",
+    ssImg2: "/images/project-single/3.jpg",
+  },
+  {
+    Id: "2",
+    pTitle: "Exterior Design",
+    slug: "Exterior-Design",
+    pImg: "/images/project/5.jpg",
+    pSImg: "/images/project-single/img-2.jpg",
+    ssImg1: "/images/project-single/2.jpg",
+    ssImg2: "/images/project-single/3.jpg",
+  },
+  {
+    Id: "3",
+    pTitle: "Interior",
+    slug: "Interior",
+    pImg: "/images/project/6.jpg",
+    pSImg: "/images/project-single/img-3.jpg",
+    ssImg1: "/images/project-single/2.jpg",
+    ssImg2: "/images/project-single/3.jpg",
+  },
+  {
+    Id: "4",
+    pTitle: "Furniture & Decor",
+    slug: "Furniture-&-Decor",
+    pImg: "/images/project/7.jpg",
+    pSImg: "/images/project-single/img-4.jpg",
+    ssImg1: "/images/project-single/2.jpg",
+    ssImg2: "/images/project-single/3.jpg",
+  },
+  {
+    Id: "5",
+    pTitle: "Modern Architecture in London",
+    slug: "Modern-Architecture-in-London",
+    pImg: "/images/project/1.jpg",
+    pSImg: "/images/project-single/img-5.jpg",
+    ssImg1: "/images/project-single/2.jpg",
+    ssImg2: "/images/project-single/3.jpg",
+  },
+  {
+    Id: "6",
+    pTitle: "Interior Design in San Francisco",
+    slug: "Interior-Design-in-San-Francisco",
+    pImg: "/images/project/2.jpg",
+    pSImg: "/images/project-single/img-6.jpg",
+    ssImg1: "/images/project-single/2.jpg",
+    ssImg2: "/images/project-single/3.jpg",
+  },
+  {
+    Id: "7",
+    pTitle: "Exclusive Home Decor in California",
+    slug: "Exclusive-Home-Decor-in-California",
+    pImg: "/images/project/3.jpg",
+    pSImg: "/images/project-single/img-7.jpg",
+    ssImg1: "/images/project-single/2.jpg",
+    ssImg2: "/images/project-single/3.jpg",
+  },
+  {
+    Id: "8",
+    pTitle: "Interior Design in San Francisco",
+    slug: "Interior-Design-in-San-Francisco",
+    pImg: "/images/project/2.jpg",
+    pSImg: "/images/project-single/img-6.jpg",
+    ssImg1: "/images/project-single/2.jpg",
+    ssImg2: "/images/project-single/3.jpg",
+  },
+];
 
 export default Projects;
